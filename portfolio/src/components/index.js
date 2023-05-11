0de017f21843b1e41c6f50463c6abac62212c14a
SCR08 <f1challenge2015@hotmail.com>
2023-05-11 18:57:50 -0500

    git commit -message 'First layout'

---
+++ portfolio/src/components/index.js
@@ -0,0 +1,7 @@
+export {default as backbutton } from './backbutton/backbutton'
+export {default as card } from './card/card'
+export {default as contactform } from './contactform/contactform'
+export {default as cover } from './cover/cover'
+export {default as navbar } from './navbar/navbar'
+export {default as tittle } from './tittle/tittle'
+export {default as topbutton } from './topbutton/topbutton'
